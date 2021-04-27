9090e994be9406ff0a0a84d59a467a311502c36a
hamza <terbouchi@gmail.com>
2021-04-27 14:52:27 +0000

    3 workshops

---
+++ vue2nd/src/components/Footer.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="container-fluid foo">
+      
+  <div class="col-lg-8 d-flex  align-items-start">
+      <h5>© 2021-Hamza Terbouchi</h5>
+  </div>
+  <div class="col-lg-4 ">
+      <ul class="social float-right">
+  <li  ><a href=""><i class="fab fa-facebook-square"></i></a></li>
+  <li  ><a href=""><i class="fab fa-instagram-square"></i></a></li>
+  <li  ><a href=""><i class="fab fa-linkedin"></i></a></li>
+  <li  ><a href=""><i class="fab fa-github-square"></i></a></li>
+</ul>
+  </div>
+      </div>
+  
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.foo{
+    background-color: #353535;
+    color: white;
+    height: auto;
+    padding: 1rem;
+}
+ul{
+    margin-top: 5px;
+    list-style: none;
+    margin-bottom: 0px;
+}
+li{
+display: inline-block;
+margin-right: 10px;
+}
+.fab{
+    font-size: 35px;
+    color: white;
+}
+</style>
